d73184955c473d838c7c0ea061e735b98d2e81a8
urooyo <inertia_mailer_0c@icloud.com>
2025-07-28 04:26:41 +0900

    fix: 아바타 모달에서 데이터 배열을 유지하며 접근 방식을 수정함 chore: 컴포넌트 파일 이름 변경.

--- components/avatarmodal.vue
+++ components/avatarmodal.vue
@@ -1,7 +1,7 @@
 <script setup>
   import { profileme } from '@/assets/info/my-profile.ts'
   const props = defineProps({
-    name: "modal",
+    name: "avatarscreen",
     emits: ["close"],
   })
 </script>
@@ -11,16 +11,16 @@
       <div class="modal-background " @click="$emit('close', false)">
         <div class="modal-ui">
           <header class="flex items-center justify-between">
-            <h3>아바타 이미지</h3>
-            <div class="flex items-center gap-2 cursor-pointer" @click="$emit('close', false)">
+            <!-- <h3>아바타 이미지</h3> -->
+            <div class="flex items-center gap-2 cursor-pointer mb-4" @click="$emit('close', false)">
               <p>닫기</p>
               <Icon name="mingcute:close-line" size="24" style="color: #E4E3ED" />
             </div>
           </header>
-          <img class="big-avatar-image" :src="profileme.avatar" />
+          <img class="big-avatar-image" :src="profileme[0].avatar" />
           <footer class="flex gap-1 pt-5 items-center place-content-center">
             <p>출처:</p>
-            <a>{{ profileme.avtrsource }}</a>
+            <a :href="profileme[0].avtrsourceurl" target="_blank">{{ profileme[0].avtrsource }}</a>
           </footer>
         </div>
       </div>
