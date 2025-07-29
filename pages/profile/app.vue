9dc840ced9d3b28bcd56b461cd350969cc218583
urooyo <inertia_mailer_0c@icloud.com>
2025-07-30 00:00:36 +0900

    chore: 이후 작업을 위해 구조 변경 시작

--- pages/profile/app.vue
+++ pages/profile/app.vue
@@ -28,11 +28,11 @@
   import { ref } from 'vue'
   import './assets/css/main.css'
   import './assets/css/font.css'
-  import { profileme } from './assets/info/my-profile.ts'
-  import { langname } from './assets/info/langname.ts'
-  import { userContent } from './assets/info/links.ts'
-  import { footerlinks } from './assets/info/footerlinks'
-  import modal from './components/avatarmodal.vue'
+  import { profileme } from '../../assets/info/my-profile.ts'
+  import { langname } from '../../assets/info/langname.ts'
+  import { userContent } from '../../assets/info/links.ts'
+  import { footerlinks } from '../../assets/info/footerlinks'
+  import modal from '../../components/avatarmodal.vue'
 
   const isModalOpen = ref(false)
 </script>
